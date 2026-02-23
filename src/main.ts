b9b4973a731e1add5866fcee28d94ec681aa8586
CarlB01 <obelisk_fallgroper00@icloud.com>
2026-02-23 17:47:00 +0100

    collapsible GroupBy with (count). Supports supercharged links.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,61 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, 
-	BasesViewRegistration} from 'obsidian';
+import {App, Modal, Plugin, BasesViewConfig, BasesAllOptions, BasesOptions} from 'obsidian';
 import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
 import { RelatedNodesView } from 'view';
 
-export const ExampleViewType = 'example-view';
+export const relatedNodesViewType = 'relatedNodesViewType';
+
+//var haha: (config: BasesViewConfig) => BasesAllOptions[]; 
+const defaultCollapseOptions: BasesOptions = {
+		type: 'text', //'text', 'property'
+		displayName: 'All collapsed (yes/no)',
+		key: 'collapsed',
+		default: 'yes',
+}
+const defaultPropertyOptions: BasesOptions = {
+		type: 'property', //'text', 'property'
+		displayName: 'default group By property',
+		key: 'tags',
+		default: 'file.tags',
+}
+
+const ignorePropertyOptions: BasesOptions = {
+		type: 'property', //'text', 'property'
+		displayName: 'ignore following properties',
+		key: 'ignore',
+		default: '#excalidraw',
+}
+
+const separatorOptions: BasesOptions = {
+		type: 'text', //'text', 'property'
+		displayName: 'separate values',
+		key: 'separator',
+		default: ' - ',
+}
 
 export default class RelatednodesPlugin extends Plugin {
 	
 	settings: MyPluginSettings;
 
 	async onload() {
-	    await this.loadSettings();
-
+		await this.loadSettings();
+		
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	
 		// Tell Obsidian about the new view type that this plugin provides.
-    	this.registerBasesView(ExampleViewType, {
-      		name: 'Example',
-      		icon: 'lucide-apple',
-      		factory: (controller, containerEl) => {
-        		return new RelatedNodesView(controller, containerEl)
-      		},
-    	});
-  	}
+		this.registerBasesView(relatedNodesViewType, {
+			name: 'Relatert',
+			icon: 'lucide-apple',
+			factory: (controller, containerEl) => 
+				new RelatedNodesView(controller, containerEl),
+			options: () => ([
+				defaultCollapseOptions, 
+				defaultPropertyOptions,
+				ignorePropertyOptions,
+				separatorOptions
+			]),
+		});
+	}
 	
 	onunload() {
 	}
--- src/view.ts
+++ src/view.ts
@@ -1,4 +1,4 @@
-import { ExampleViewType } from 'main';
+import { relatedNodesViewType } from 'main';
 import {App, 
     PluginSettingTab, 
     Setting, 
@@ -8,24 +8,154 @@ import {App,
     Notice, 
     Plugin,     
     BasesView,
+    setIcon,
+    ViewState,
     BasesViewRegistration,
     QueryController,
+    HoverParent,
+    HoverPopover,
+    parsePropertyId,
+    Keymap,
 } from 'obsidian';
 
-export class RelatedNodesView extends BasesView {
-  readonly type = ExampleViewType;
+
+export class RelatedNodesView extends BasesView implements HoverParent {
+  hoverPopover: HoverPopover | null;
+
+  readonly type = relatedNodesViewType;
   private containerEl: HTMLElement;
 
   constructor(controller: QueryController, parentEl: HTMLElement) {
     super(controller);
-    this.containerEl = parentEl.createDiv('bases-example-view-container');
+    this.containerEl = parentEl.createDiv('bases-relatednotes-view-container');
   }
 
   // onDataUpdated is called by Obsidian whenever there is a configuration
-  // or data change in the vault which may affect your view. For now,
-  // simply draw "Hello World" to screen.
+  // or data change in the vault which may affect your view.
   public onDataUpdated(): void {
+    const { app } = this;
+    const order = this.config.getOrder()
+    const defaultCollapsed = (String(this.config.get('collapsed')) || 'yes' == 'yes') ? true : false;
+    const propertySeparator = String(this.config.get('separator')) || ' - ';
+    
+    // Clear entries created by previous iterations. Remember, you should
+    // instead attempt element reuse when possible.
     this.containerEl.empty();
-    this.containerEl.createDiv({ text: 'Hello World' });
+
+    const btnCollapseAll = this.containerEl.createDiv(defaultCollapsed
+      ? 'bases-relatednodes-btn-collapse-all collapsed'
+      : 'bases-relatednodes-btn-collapse-all');
+    setIcon(btnCollapseAll, defaultCollapsed ? 'chevrons-right': 'chevrons-down'); 
+    
+    btnCollapseAll.addEventListener('click', () => {
+      const collapsed = btnCollapseAll.hasClass('collapsed');
+      btnCollapseAll.toggleClass('collapsed', !collapsed);
+      setIcon(btnCollapseAll, collapsed ? 'chevrons-down' : 'chevrons-right')
+      this.containerEl.querySelectorAll(':scope > div').forEach(
+        (x) => { 
+          x.toggleClass('collapsed', !collapsed);
+          console.log("x ", x.className);
+          x.querySelectorAll(':scope > ul').forEach(
+            (y) => { 
+              y.toggleClass('collapsed', !collapsed);
+              //const myElement = y.querySelector('.bases-relatednodes-group-heading-icon') as HTMLElement; 
+              //setIcon(myElement, collapsed ? 'chevron-down' : 'chevron-right')
+              console.log("y ", y.className);
+            }
+          )
+        }
+      )
+    });
+      
+
+    // this.data contains both grouped and ungrouped versions of the data.      
+    for (const group of this.data.groupedData) {
+      const groupEl = this.containerEl.createDiv('bases-relatednodes-group');
+      const groupHeading = groupEl.createDiv('bases-relatednodes-group-heading');
+      const iconEl = groupHeading.createSpan('bases-relatednodes-group-heading-icon');
+      setIcon(iconEl, defaultCollapsed ? 'chevron-right': 'chevron-down'); 
+      const groupKey = (group.key?.toString() ?? "").replace(',', '');
+      groupHeading.createSpan({ 
+        text: (groupKey || "(ingen #)") 
+              + " (" + group.entries.length + ")"
+      });
+      const groupUl = groupEl.createEl('ul', { 
+        cls: defaultCollapsed 
+          ? "bases-relatednodes-group-ul collapsed"
+          : "bases-relatednodes-group-ul"});
+      
+      groupHeading.addEventListener('click', () => {
+        const collapsed = groupUl.hasClass('collapsed');
+        groupUl.toggleClass('collapsed', !collapsed);
+        setIcon(iconEl, collapsed ? 'chevron-down' : 'chevron-right')
+        groupHeading.parentElement?.querySelectorAll(':scope > ul').forEach(
+            (x) => { x.toggleClass('collapsed', !collapsed) }
+        )
+      });
+        
+      for (const entry of group.entries) {
+        groupUl.createEl('li', 'bases-list-entry', (el) => {
+          let firstProp = true;
+          for (const propertyName of order) {
+            const { type, name } = parsePropertyId(propertyName);
+            const value = entry.getValue(propertyName);
+
+            if (value == null || value == undefined)  continue;
+  
+            if (!firstProp) {
+              el.createSpan({
+                cls: 'bases-list-separator',
+                text: propertySeparator
+              });
+            }
+            firstProp = false;
+            // If the `file.name` property is included in the order, render
+            // it specially so that it links to that file.
+            if (name === 'name' && type === 'file') {
+              const fileName = String(entry.file.name);
+              const baseName = String(entry.file.basename);
+              const filePath = String(entry.file.path);
+              console.log(groupKey);
+              const linkEl = el.createEl('a', { 
+                text: fileName,
+                cls: "internal-link data-link-icon data-link-icon-after data-link-text",
+                attr: {
+                  'data-href': fileName,
+                  'draggable': 'true',
+                  'data-link-tags': groupKey, // '#han #lege #psykiater #phD',
+                  'data-link-data-href': baseName,
+                  'data-link-path': filePath
+                }
+              });
+              linkEl.onClickEvent((evt) => {
+                if (evt.button !== 0 && evt.button !== 1) return;
+                evt.preventDefault();
+                const modEvent = Keymap.isModEvent(evt);
+                void app.workspace.openLinkText(filePath, '', modEvent);
+              });
+  
+              linkEl.addEventListener('mouseover', (evt) => {
+                app.workspace.trigger('hover-link', {
+                  event: evt,
+                  source: 'bases',
+                  hoverParent: this,
+                  targetEl: linkEl,
+                  linktext: filePath,
+                });
+              });
+            }
+            // For all other properties, just display the value as text.
+            // In your view you may also choose to use the `Value.renderTo`
+            // API to better support photos, links, icons, etc.
+            else {
+              el.createSpan({
+                cls: 'bases-list-entry-property',
+                text: value!.toString()
+              });
+            }
+          }
+        });
+      }
+    }
   }
 }
